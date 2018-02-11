30e08a64ae90d24efc25a1d72f50e558b9085b34
Yacine Petitprez <yacine.petitprez@ingedata.net>
2018-02-11 15:04:09 +0800

    Fix

--- src/tree_template/components.cr
+++ src/tree_template/components.cr
@@ -2,5 +2,4 @@
 # To add a component, just define a new method, and you're good to go!
 # Each methods will be available to the template engine after that :)
 module TreeTemplate::Components
-  extend self
 end
