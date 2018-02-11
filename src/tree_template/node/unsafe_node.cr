bbaa56b9558104861953e64e2d892f7d3b02f9b2
Yacine Petitprez <yacine.petitprez@ingedata.net>
2018-02-11 15:22:10 +0800

    Fix mistake

--- src/tree_template/node/unsafe_node.cr
+++ src/tree_template/node/unsafe_node.cr
@@ -1,4 +1,4 @@
-class TreeTemplate::TextNode < TreeTemplate::Node
+class TreeTemplate::UnsafeNode < TreeTemplate::Node
   def initialize(@text : String); end
 
   def render(formatter : TreeTemplate::Formatter, page : TreeTemplate? = nil)
