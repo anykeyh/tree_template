64d5dcf0f51f5d1c6a3bd5c93eeb01683fe62367
Yacine Petitprez <yacine.petitprez@ingedata.net>
2018-02-11 12:41:23 +0800

    Finalization of components system. Quite simple !

---
+++ src/tree_template/components.cr
@@ -0,0 +1,5 @@
+# Add to TreeTemplate::Components your differents components
+# To add a component, just define a new method, and you're good to go!
+# Each methods will be available to the template engine after that :)
+module TreeTemplate::Components
+end
